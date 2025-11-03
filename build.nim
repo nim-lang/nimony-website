89753d7a7a0e9598810b0a9c66a73e62658017d3
araq <araq4k@proton.me>
2025-11-03 08:19:07 +0100

    added build.nim for convenient testing

---
+++ build.nim
@@ -0,0 +1,18 @@
+## Build the website for local testing purposes
+
+import std / os
+
+proc exec(cmd: string) =
+  if execShellCmd(cmd) != 0:
+    quit "FAILURE: " & cmd
+
+copyDir "../nimony", "nimony"
+try:
+  createDir "site"
+  copyFile "style.css", "site/style.css"
+  copyFile "script.js", "site/script.js"
+  exec "nim c -r nimony/tools/dagon.nim nimony/doc/stdlib.dgn.md"
+  exec "nim md2html -o:site/index.html nimony/doc/manual.md"
+  exec "nim c -r multipage.nim site/index.html"
+finally:
+  removeDir "nimony"
