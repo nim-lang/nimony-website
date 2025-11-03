1186e2917b37fa7720663ebcdaf2457f07f1d585
araq <araq4k@proton.me>
2025-11-03 12:15:33 +0100

    multipage layout

--- build.nim
+++ build.nim
@@ -1,4 +1,5 @@
-## Build the website for local testing purposes
+## Build the website. Compile with `-d:local` for a local build
+## that uses the Nimony sources from the sibling directory.
 
 import std / os
 
@@ -6,13 +7,28 @@ proc exec(cmd: string) =
   if execShellCmd(cmd) != 0:
     quit "FAILURE: " & cmd
 
-copyDir "../nimony", "nimony"
+proc buildArticles =
+  for file in walkFiles("articles/*.md"):
+    let dest = file.splitFile.name
+    exec "nim md2html -o:site/" & dest & ".html " & file
+
+when defined(local):
+  copyDir "../nimony", "nimony"
+
+try:
+  removeDir "site"
+except:
+  discard "fine"
 try:
   createDir "site"
+  buildArticles()
   copyFile "style.css", "site/style.css"
   copyFile "script.js", "site/script.js"
   exec "nim c -r nimony/tools/dagon.nim nimony/doc/stdlib.dgn.md"
-  exec "nim md2html -o:site/manual.html nimony/doc/manual.md"
+  exec "nim md2html -d:man -o:site/manual.html nimony/doc/manual.md"
+  exec "nim md2html -o:site/index.html content/index.md"
+
   exec "nim c -r multipage.nim site/manual.html"
 finally:
-  removeDir "nimony"
+  when defined(local):
+    removeDir "nimony"
---
+++ content/index.md
@@ -0,0 +1,12 @@
+# Nimony
+## Efficient, expressive, elegant
+
+Nimony is new compiler that will evolve to become Nim 3.
+
+Nim is a statically typed compiled systems programming language. It combines successful concepts from mature languages like Python, Ada and Modula 3.
+
+
+# News
+
+We have our first release! Version 0.2! Read this [article](version0_2.html) for more information.
+
